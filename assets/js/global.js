542ccac91853d9789b55835624ecdd50a6a7cb2d
Dilmurat777 <mrdilmurat777@gmail.com>
2023-11-17 20:35:39 +0600

    first commit

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Add event on many elements
+
+const addEventOnElements = function(elements, eventType, callback) {
+	for (const elem of elements) elem.addEventListener(eventType, callback);
+}
+
+const searchBox = document.querySelector('[search-box]');
+const searchTogglers = document.querySelectorAll('[search-toggler]');
+
+addEventOnElements(searchTogglers, 'click', function() {
+	searchBox.classList.toggle('active')
+})
